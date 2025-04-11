c1859ab0dac0a886c868390dca222bd6aac9eb20
Ieuan Priede <priedeieuan@gmail.com>
2025-04-11 07:46:59 +0000

    Adding Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
+  
     var elems = document.querySelectorAll('.modal');
     M.Modal.init(elems);
-  }); 
+
+    // datepicker initialization  
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: 'Select'}
+    });
+
+    // select initialization    
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+}); 
